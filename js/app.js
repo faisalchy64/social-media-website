dbd1932ce159fe1e631db969116b0c166f41d6b2
Faisal Karim <faisalchy64@gmail.com>
2022-03-11 12:43:00 +0600

    complete work

--- js/app.js
+++ js/app.js
@@ -17,7 +17,10 @@ const isLiked = (id) => {
 
 const addToLiked = (id) => {
     likedPostsId.push(id);
-    showPosts(posts);
+    const remainingPosts = posts.filter(
+        (post) => !reportedPostsId.includes(post.id)
+    );
+    showPosts(remainingPosts);
 };
 
 const reportPost = (id) => {
